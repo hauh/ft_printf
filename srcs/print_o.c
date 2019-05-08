ea29add6fc566fb2b911871b0e107e4478e897ce
Stefany Morty <smorty@mi-t6.21-school.ru>
2019-05-08 17:59:03 +0300

    macro MAXMIN

--- srcs/print_o.c
+++ srcs/print_o.c
@@ -6,7 +6,7 @@
 /*   By: smorty <smorty@student.21school.ru>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/06 16:58:45 by smorty            #+#    #+#             */
-/*   Updated: 2019/05/07 19:33:36 by smorty           ###   ########.fr       */
+/*   Updated: 2019/05/08 17:36:28 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ static void	otoa_flag(char *s, uintmax_t n, t_frmt *params, int size)
 	int len;
 	int hash;
 
-	precision = ((*params).precision > 0 ? (*params).precision : 1);
+	precision = (*params).precision - (*params).flag_prec;
 	hash = (*params).hash;
 	len = num_len_o(n);
 	if ((*params).minus)
@@ -69,21 +69,19 @@ int			print_o(uintmax_t n, t_frmt *params)
 {
 	char	*s;
 	int		size;
-	int		hash;
 	int		printed;
 	int		precision;
 
-	if ((*params).zero && ((*params).minus || (*params).precision >= 0))
+	if ((*params).zero && ((*params).minus || (*params).flag_prec))
 		(*params).zero = 0;
-	precision = (*params).precision;
-	hash = (*params).hash;
-	size = (!n && precision ? 1 : num_len_o(n) + hash);
-	size = find_max(size, (*params).width, precision + hash);
+	precision = (*params).precision - (*params).flag_prec;
+	size = MAX(num_len_o(n), precision) + (*params).hash;
+	size = MAX(size, (*params).width);
 	if (!(s = (char *)malloc(sizeof(char) * (size + 1))))
 		return (-1);
 	ft_memset(s, ((*params).zero ? '0' : ' '), size);
 	*(s + size) = 0;
-	if (n || precision || hash)
+	if (n || precision || (*params).hash)
 		otoa_flag(s, n, params, size);
 	printed = write(1, s, size);
 	free(s);
