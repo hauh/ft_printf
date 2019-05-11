91dab8ed8e44ad33e4fde8e39b23d700bc3acf58
Stefany Morty <smorty@mi-t4.21-school.ru>
2019-05-11 23:04:54 +0300

    asterisk

--- srcs/parse_format.c
+++ srcs/parse_format.c
@@ -6,7 +6,7 @@
 /*   By: smorty <smorty@student.21school.ru>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 16:11:46 by smorty            #+#    #+#             */
-/*   Updated: 2019/05/10 20:43:30 by smorty           ###   ########.fr       */
+/*   Updated: 2019/05/11 22:49:15 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,6 @@
 
 static void	initialize_struct(t_frmt *params)
 {
-/*	(*params).minus = 0;
-	(*params).plus = 0;
-	(*params).space = 0;
-	(*params).hash = 0;
-	(*params).zero = 0;
-	(*params).flag_prec = 0;*/
 	(*params).flags = 0;
 	(*params).mod = 0;
 	(*params).width = 0;
@@ -31,20 +25,15 @@ static void	initialize_struct(t_frmt *params)
 static void	get_flag(const char **format, int *flags)
 {
 	if (**format == '-')
-//		(*params).minus = 1;
 		*flags |= F_MINUS;
 	else if (**format == '+')
 		*flags |= F_PLUS;
-//		(*params).plus = 1;
 	else if (**format == ' ')
 		*flags |= F_SPACE;
-//		(*params).space = 1;
 	else if (**format == '#')
 		*flags |= F_HASH;
-//		(*params).hash = 1;
 	else if (**format == '0' && !ft_isdigit(*(*format - 1)))
 		*flags |= F_ZERO;
-//		(*params).zero = 1;
 	(*format)++;
 }
 
@@ -101,6 +90,25 @@ int			parse_format(const char **format, va_list argp)
 			params.precision = ft_atoi(*format) + 1;
 			while (ft_isdigit(**format))
 				(*format)++;
+		}
+		else if (**format == '*')
+		{
+			if (*(*format - 1) == '.')
+			{
+				params.flags |= F_PREC;
+				params.precision = va_arg(argp, int) + 1;
+				(*format)++;
+			}
+			else
+			{
+				params.width = va_arg(argp, int);
+				if (params.width < 0)
+				{
+					params.width = ~params.width + 1;
+					params.flags |= F_MINUS;
+				}
+				(*format)++;
+			}
 		}
 		else if (MOD(**format))
 			get_mod(format, &params);
