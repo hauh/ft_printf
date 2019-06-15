ac6008e40c3d1e838ecd07b4db6c9030e16ef1e8
smorty <smorty@21-school.ru>
2019-06-15 20:18:45 +0300

    colors bugfix

--- srcs/colors.c
+++ srcs/colors.c
@@ -6,7 +6,7 @@
 /*   By: smorty <smorty@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 17:37:46 by smorty            #+#    #+#             */
-/*   Updated: 2019/05/31 20:20:09 by smorty           ###   ########.fr       */
+/*   Updated: 2019/06/15 20:18:28 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,17 @@ void		check_color_and_fd(const char **format, va_list *argp)
 	if (ft_strnequ(*format, "{red}", 5))
 		*format += set_color(COLOR_RED, 5);
 	else if (ft_strnequ(*format, "{green}", 7))
-		*format += set_color(COLOR_GREEN, 7);
+		*format += set_color(COLOR_GREEN, 5);
 	else if (ft_strnequ(*format, "{yellow}", 8))
-		*format += set_color(COLOR_YELLOW, 8);
+		*format += set_color(COLOR_YELLOW, 5);
 	else if (ft_strnequ(*format, "{blue}", 6))
-		*format += set_color(COLOR_BLUE, 6);
+		*format += set_color(COLOR_BLUE, 5);
 	else if (ft_strnequ(*format, "{magenta}", 9))
-		*format += set_color(COLOR_MAGENTA, 9);
+		*format += set_color(COLOR_MAGENTA, 5);
 	else if (ft_strnequ(*format, "{cyan}", 6))
-		*format += set_color(COLOR_CYAN, 6);
+		*format += set_color(COLOR_CYAN, 5);
 	else if (ft_strnequ(*format, "{eoc}", 5))
-		*format += set_color(COLOR_RESET, 5);
+		*format += set_color(COLOR_RESET, 4);
 	else if (ft_strnequ(*format, "{*}", 3))
 	{
 		print_buf();
